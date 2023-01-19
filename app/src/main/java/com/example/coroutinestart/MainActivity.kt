09b5d10cac8c3f68446598950e169c4117e5fdcc
Ivan Usatov <andrappdevelop@gmail.com>
2023-01-19 18:24:33 +0700

    Add layout, coroutines

--- app/src/main/java/com/example/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.coroutinestart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import com.example.coroutinestart.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.buttonLoad.setOnClickListener {
+            lifecycleScope.launch {
+                loadData()
+            }
+        }
     }
+
+    private suspend fun loadData() {
+        binding.progressBar.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        val city = loadCity()
+        binding.textViewLocation.text = city
+        val temperature = loadTemperature(city)
+        binding.textViewTemperature.text = temperature.toString()
+        binding.progressBar.isVisible = false
+        binding.buttonLoad.isEnabled = true
+    }
+
+    private suspend fun loadCity(): String {
+        delay(5000)
+        return "Tomsk"
+    }
+
+    private suspend fun loadTemperature(city: String): Int {
+        Toast.makeText(
+            this,
+            "Loading temperature for city: $city",
+            Toast.LENGTH_SHORT
+        ).show()
+        delay(5000)
+        return -17
+    }
+
 }
